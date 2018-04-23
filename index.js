d98aed44ecf0e55700c4302e64cfe271c0211cdf
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-23 18:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  return drivers.filter((driver.toLowerCase()) => driver == (string.toLowerCase());
+  return drivers.filter((driver.toLowerCase()) => driver == (string.toLowerCase()));
 }

0c27a4af6a946f79cc3cc447793995e25f0a1afe
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-23 18:32:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  return drivers.filter(driver => (driver.toLowerCase()) == (string.toLowerCase()));
+  return drivers.filter(driver => (driver.toLowerCase()) === (string.toLowerCase()));
 }
 
 function fuzzyMatch(drivers, string) {
-  return [];
+  return drivers.filter(driver => driver.slice(0,2) === string);
 }
